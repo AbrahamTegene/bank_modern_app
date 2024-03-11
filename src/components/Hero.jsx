9a67686dc25b5ace20afa4fc6228bae2ad36314d
Abrish Tegu <abrahamtegene1990@gmail.com>
2024-03-11 20:17:16 +0300

    Hero section is updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,57 @@
-import React from 'react'
-import styles from '../style';
-import { discount, robot} from '../assets/'
-import GetStarted from './GetStarted'
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-   <section id='home' className={`flex md:flex-row flex-col $(styles.paddingY)`}>
-      <div className='flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6'>
-        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded=[10px] mb-2'>
-          <img src={discount} alt="discount" className='w-[32px] h-[32px]'/>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      {/* the first paragraph part */}
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-               <span className='text-white'>20%</span> Discount For {''} <span className='text-white'>1 month</span> Account
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
           </p>
         </div>
 
-        <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
-            The  Next <br className='sm:block hidden'/> {" "}    
-            <span className='text-gradient'>Generation </span> { " " }
-            
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
           </h1>
 
-          {/* payment method */}
-          <div className='ss:flex hidden md:mr-4 mr-0'>
-            <GetStarted/>
+          {/* image part */}
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
           </div>
         </div>
 
-      {/* this is a section which is contain states */}
-
-        <h1 className=' font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
-        Payment Method </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5}`}> Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
       </div>
 
-      {/* image show  */}
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-        {/* self close div */}
-        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
-        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient'/>
-        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20  blue__gradient'/>
+        {/* gradient(color) start  */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient(color) end */}
       </div>
 
-      {/* for iphone 12 get start is not shown we handdle this way */}
       <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted/>
+        <GetStarted />
       </div>
-   </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
